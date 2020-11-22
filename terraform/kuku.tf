38ae87de4a1a17a6b4786947660e6837fe5e4920
Or Evron <orevron@gmail.com>
2020-11-22 14:48:47 +0200

    test

--- terraform/kuku.tf
+++ terraform/kuku.tf
@@ -2,17 +2,7 @@ resource "aws_s3_bucket" "template_bucket" {
   bucket        = "local.bucket_name"
   force_destroy = true
 
-  versioning {
-    enabled = true
-  }
   tags = {
     Name = "local.bucket_name-data.aws_caller_identity.current.account_id"
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
   }
 }
