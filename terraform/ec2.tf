d565a456cab427e2749b93a96a9b921ff3e56b8c
orevron <orevron@gmail.com>
2021-04-06 17:16:16 +0300

    kisi kisi

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "web_host" {
+
   # ec2 have plain text secrets in user data
   ami           = "${var.ami}"
   instance_type = "t2.nano"
@@ -23,9 +24,10 @@ EOF
 }
 
 resource "aws_ebs_volume" "web_host_storage" {
+
   # unencrypted volume
   availability_zone = "${var.availability_zone}"
-  #encrypted         = false  # Setting this causes the volume to be recreated on apply 
+  #encrypted         = false  # Setting this causes the volume to be recreated on apply
   size              = 1
   tags = {
     Name = "${local.resource_prefix.value}-ebs"
@@ -33,6 +35,7 @@ resource "aws_ebs_volume" "web_host_storage" {
 }
 
 resource "aws_ebs_snapshot" "example_snapshot" {
+
   # ebs snapshot without encryption
   volume_id   = "${aws_ebs_volume.web_host_storage.id}"
   description = "${local.resource_prefix.value}-ebs-snapshot"
@@ -42,12 +45,14 @@ resource "aws_ebs_snapshot" "example_snapshot" {
 }
 
 resource "aws_volume_attachment" "ebs_att" {
+
   device_name = "/dev/sdh"
   volume_id   = "${aws_ebs_volume.web_host_storage.id}"
   instance_id = "${aws_instance.web_host.id}"
 }
 
 resource "aws_security_group" "web-node" {
+
   # security group is open to the world in SSH port
   name        = "${local.resource_prefix.value}-sg"
   description = "${local.resource_prefix.value} Security Group"
@@ -78,6 +83,7 @@ resource "aws_security_group" "web-node" {
 }
 
 resource "aws_vpc" "web_vpc" {
+
   cidr_block           = "172.16.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
@@ -87,6 +93,7 @@ resource "aws_vpc" "web_vpc" {
 }
 
 resource "aws_subnet" "web_subnet" {
+
   vpc_id                  = aws_vpc.web_vpc.id
   cidr_block              = "172.16.10.0/24"
   availability_zone       = var.availability_zone
@@ -98,6 +105,7 @@ resource "aws_subnet" "web_subnet" {
 }
 
 resource "aws_subnet" "web_subnet2" {
+
   vpc_id                  = aws_vpc.web_vpc.id
   cidr_block              = "172.16.11.0/24"
   availability_zone       = var.availability_zone2
@@ -110,6 +118,7 @@ resource "aws_subnet" "web_subnet2" {
 
 
 resource "aws_internet_gateway" "web_igw" {
+
   vpc_id = aws_vpc.web_vpc.id
 
   tags = {
@@ -118,6 +127,7 @@ resource "aws_internet_gateway" "web_igw" {
 }
 
 resource "aws_route_table" "web_rtb" {
+
   vpc_id = aws_vpc.web_vpc.id
 
   tags = {
@@ -126,16 +136,19 @@ resource "aws_route_table" "web_rtb" {
 }
 
 resource "aws_route_table_association" "rtbassoc" {
+
   subnet_id      = aws_subnet.web_subnet.id
   route_table_id = aws_route_table.web_rtb.id
 }
 
 resource "aws_route_table_association" "rtbassoc2" {
+
   subnet_id      = aws_subnet.web_subnet2.id
   route_table_id = aws_route_table.web_rtb.id
 }
 
 resource "aws_route" "public_internet_gateway" {
+
   route_table_id         = aws_route_table.web_rtb.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.web_igw.id
@@ -147,6 +160,7 @@ resource "aws_route" "public_internet_gateway" {
 
 
 resource "aws_network_interface" "web-eni" {
+
   subnet_id   = aws_subnet.web_subnet.id
   private_ips = ["172.16.10.100"]
 
@@ -157,6 +171,7 @@ resource "aws_network_interface" "web-eni" {
 
 # VPC Flow Logs to S3
 resource "aws_flow_log" "vpcflowlogs" {
+
   log_destination      = aws_s3_bucket.flowbucket.arn
   log_destination_type = "s3"
   traffic_type         = "ALL"
@@ -169,6 +184,7 @@ resource "aws_flow_log" "vpcflowlogs" {
 }
 
 resource "aws_s3_bucket" "flowbucket" {
+
   bucket = "${local.resource_prefix.value}-flowlogs"
   force_destroy = true
 
@@ -179,21 +195,25 @@ resource "aws_s3_bucket" "flowbucket" {
 }
 
 output "ec2_public_dns" {
+
   description = "Web Host Public DNS name"
   value       = aws_instance.web_host.public_dns
 }
 
 output "vpc_id" {
+
   description = "The ID of the VPC"
   value       = aws_vpc.web_vpc.id
 }
 
 output "public_subnet" {
+
   description = "The ID of the Public subnet"
   value       = aws_subnet.web_subnet.id
 }
 
 output "public_subnet2" {
+
   description = "The ID of the Public subnet"
   value       = aws_subnet.web_subnet2.id
 }
