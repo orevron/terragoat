43997af5e59f0ac2319d22a857193ee8e347572e
Or Evron <orevron@gmail.com>
2021-10-07 10:12:43 +0300

    test

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -24,6 +24,7 @@ EOF
 
 resource "aws_ebs_volume" "web_host_storage" {
   # unencrypted volume
+  
   availability_zone = "${var.availability_zone}"
   #encrypted         = false  # Setting this causes the volume to be recreated on apply 
   size              = 1
