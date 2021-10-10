b1eefdebd26ca6dc2bee1f157d14a94be5bc034a
Or Evron <orevron@gmail.com>
2021-10-10 14:48:19 +0300

    one violation file

--- terraform/s4.tf
+++ terraform/s4.tf
@@ -1,21 +1,3 @@
-resource "aws_iam_user_policy" "userpolicy" {
-  name = "excess_policy"
-  user = "${aws_iam_user.user.name}"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:*",
-        "s3:*",
-        "lambda:*",
-        "cloudwatch:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
-}
-EOF
+resource "aws_sns_topic" "example" {
+  kms_master_key_id = "value"
 }
