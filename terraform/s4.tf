a5c333895d76821499483c385fabd63159109a9b
Or Evron <20145882+orevron@users.noreply.github.com>
2021-10-12 02:38:27 -0700

    Update s4.tf

--- terraform/s4.tf
+++ terraform/s4.tf
@@ -1,5 +1,7 @@
 resource "aws_sns_topic" "example" {
-    kms_master_key_id = "value"
-    
-    
+    kms_master_key_id = "value"    
+    tags = {
+      Environment = "dev"
+      Owner = "apps-team"
+    }
 }
