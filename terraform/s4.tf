3cbdbade5b1332d9b25e48405afc0804ada9e5ac
Or Evron <orevron@gmail.com>
2021-10-17 12:49:19 +0300

    kisi

--- terraform/s4.tf
+++ terraform/s4.tf
@@ -7,7 +7,10 @@ resource "aws_sns_topic" "example1" {
 }
 
 resource "aws_sns_topic" "example2" {
-  
+  tags              = {
+    Environment = "dev"
+    Owner       = "apps-team"
+  }
 }
 
 resource "aws_sns_topic" "example3" {
