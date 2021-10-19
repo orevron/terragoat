fa8342d927745a512446b40c198692990934890b
Or Evron <orevron@gmail.com>
2021-10-19 16:25:42 +0300

    asd

--- terraform/s4.tf
+++ terraform/s4.tf
@@ -1,22 +1,23 @@
-resource "aws_iam_user_policy" "userpolicy" {
-  name = "excess_policy"
-  user = "${aws_iam_user.user.name}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:*",
-        "s3:*",
-        "lambda:*",
-        "cloudwatch:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
+resource "aws_sns_topic" "example1" {
+  kms_master_key_id = "value"
+  tags              = {
+    Environment = "dev"
+    Owner       = "apps-team"
+  }
 }
-EOF
+
+resource "aws_sns_topic" "example2" {
+  kms_master_key_id = "value"
+  tags              = {
+    Environment = "dev"
+    Owner       = "apps-team"
+  }
+}
+
+resource "aws_sns_topic" "example3" {
+  kms_master_key_id = "value"
+  tags              = {
+    Environment = "dev"
+    Owner       = "apps-team"
+  }
 }
