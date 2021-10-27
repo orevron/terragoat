da0c886dbbeb66142b7a09730a8150ee26d2384d
Or Evron <orevron@gmail.com>
2021-10-27 13:55:00 +0300

    errorrrrrr

--- terraform/s4.tf
+++ terraform/s4.tf
@@ -7,11 +7,6 @@ resource "aws_sns_topic" "example1" {
 }
 
 resource "aws_sns_topic" "example2" {
-  kms_master_key_id = "value"
- tags              = {
-    Environment = "dev"
-    Owner       = "apps-team"
-  }
 }
 
 resource "aws_sns_topic" "example3" {
