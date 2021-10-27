671bf7cfee5533aa64dbb8e4581e976131d724d0
Or Evron <20145882+orevron@users.noreply.github.com>
2021-10-26 23:25:20 -0700

    kakakaka

--- terraform/s4.tf
+++ terraform/s4.tf
@@ -8,7 +8,10 @@ resource "aws_sns_topic" "example1" {
 
 resource "aws_sns_topic" "example2" {
   kms_master_key_id = "value"
-
+ tags              = {
+    Environment = "dev"
+    Owner       = "apps-team"
+  }
 }
 
 resource "aws_sns_topic" "example3" {
