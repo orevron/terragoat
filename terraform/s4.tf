61d2accee300ac8c313218efaa36ce6dcea0a765
Or Evron <orevron@gmail.com>
2021-12-21 13:03:59 +0200

    add file revert s4.tf

--- terraform/s4.tf
+++ terraform/s4.tf
@@ -6,26 +6,10 @@ resource "aws_sns_topic" "example1" {
   }
 }
 
+resource "aws_sns_topic" "example2" {
 
-resource "aws_s3_bucket" "data2" {
-  bucket        = "${local.resource_prefix.value}-data"
-  acl           = "public-read"
-  force_destroy = true
-  tags          = {
-    Name                 = "${local.resource_prefix.value}-data"
-    Environment          = local.resource_prefix.value
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
-    git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
-    git_modifiers        = "nimrodkor"
-    git_org              = "try-bridgecrew"
-    git_repo             = "terragoat"
-    yor_trace            = "fc8c2d7a-1997-4fc2-95c1-277cba5c2a38"
-  }
-  versioning {
+  kms_master_key_id = "value"
 
-  }
 }
 
 resource "aws_sns_topic" "example3" {
