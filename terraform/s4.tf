605fde19ff0f2f206f3ef80c42c2f275338894fd
Or Evron <orevron@gmail.com>
2021-12-21 12:53:54 +0200

    fix bucket

--- terraform/s4.tf
+++ terraform/s4.tf
@@ -6,9 +6,7 @@ resource "aws_sns_topic" "example1" {
   }
 }
 
-
 resource "aws_s3_bucket" "data2" {
-  
   bucket        = "${local.resource_prefix.value}-data"
   acl           = "public-read"
   force_destroy = true
@@ -28,8 +26,6 @@ resource "aws_s3_bucket" "data2" {
 
   }
 }
-
-
 
 resource "aws_sns_topic" "example3" {
   kms_master_key_id = "value"
