bc7fd0ec3602347f1bbee3626d87296f8a2d0da3
Or Evron <20145882+orevron@users.noreply.github.com>
2022-02-16 14:43:27 +0200

    Create custom.tf

---
+++ custom.tf
@@ -0,0 +1,8 @@
+resource "aws_s3_bucket" "data" {
+  bucket        = "${local.resource_prefix.value}-data"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
+}
