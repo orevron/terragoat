a07fee1ad0eb037b62b2fd5f04c644f0e4020b56
Or Evron <20145882+orevron@users.noreply.github.com>
2022-05-01 13:03:32 +0300

    Update s5.tf

--- terraform/s5.tf
+++ terraform/s5.tf
@@ -1,25 +1,18 @@
-resource "aws_s3_bucket" "logs" {
-}
-
-
-resource "aws_s3_bucket_versioning" "logs" {
-  bucket = aws_s3_bucket.logs.id
-
-  versioning_configuration {
-    status = "Enabled"
-  bucket = "${local.resource_prefix.value}-logs"
-  acl    = "log-delivery-write"
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm     = "aws:kms"
-        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
-      }
-    }
-  }
+resource "aws_s3_bucket" "data123456" {
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
   force_destroy = true
-  tags = {
-    Name        = "${local.resource_prefix.value}-logs"
+  tags = merge({
+    Name        = "${local.resource_prefix.value}-data"
     Environment = local.resource_prefix.value
-  }
+    }, {
+    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+    git_file             = "terraform/aws/s3.tf"
+    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_by = "nimrodkor@gmail.com"
+    git_modifiers        = "nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "0874007d-903a-4b4c-945f-c9c233e13243"
+  })
 }
