34e027a39a757b81f670b647a004036ec1aa3f04
Or Evron <20145882+orevron@users.noreply.github.com>
2022-05-01 12:42:16 +0300

    Create s5.tf

---
+++ terraform/s5.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "logs" {
+  bucket = "${local.resource_prefix.value}-logs"
+  acl    = "log-delivery-write"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = "${aws_kms_key.logs_key.arn}"
+      }
+    }
+  }
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-logs"
+    Environment = local.resource_prefix.value
+  }
+}
