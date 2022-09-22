59f1ebb9a0f5b6345f9f060ad2b9e2d02d9ac287
Or Evron <orevron@gmail.com>
2022-09-22 14:21:57 +0300

    add directories

---
+++ test/s4.tf
@@ -0,0 +1,17 @@
+resource "aws_sns_topic" "example1" {
+  kms_master_key_id = "value"
+  tags              = {
+    Environment = "dev"
+    Owner       = "apps-team"
+  }
+}
+resource "aws_sns_topic" "example2" {
+}
+
+resource "aws_sns_topic" "example3" {
+  kms_master_key_id = "value"
+  tags              = {
+    Environment = "dev"
+    Owner       = "apps-team"
+  }
+}
