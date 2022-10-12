3e44f3aa33f0b9ae8e9cd6794d9511c8f624c607
Or Evron <20145882+orevron@users.noreply.github.com>
2022-10-12 13:26:00 +0300

    Update s5.tf

--- terraform/s5.tf
+++ terraform/s5.tf
@@ -3,13 +3,6 @@ resource "aws_s3_bucket" "data123456" {
   acl           = "public-read"
   force_destroy = true
   tags = merge({
-    Name        = "${local.resource_prefix.value}-data"
-    Environment = local.resource_prefix.value
-    }, {
-    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
-    git_file             = "terraform/aws/s3.tf"
-    git_last_modified_at = "2020-06-16 14:46:24"
-    git_last_modified_by = "nimrodkor@gmail.com"
     git_modifiers        = "nimrodkor"
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
