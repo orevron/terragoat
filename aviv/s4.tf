e5b34c8b5073cc5ab0e8c17529a77ce8c862e3ef
Or Evron <20145882+orevron@users.noreply.github.com>
2024-04-30 14:25:50 +0300

    Update s4.tf

--- aviv/s4.tf
+++ aviv/s4.tf
@@ -5,8 +5,20 @@ resource "aws_sns_topic" "example1" {
     Owner       = "apps-team"
   }
 }
-resource "aws_sns_topic" "example2" {
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket        = "${local.resource_prefix.value}-data"
+  acl           = "public-read"
+  force_destroy = true
+  tags = {
+    Name        = "${local.resource_prefix.value}-data"
+    Environment = local.resource_prefix.value
+  }
 }
+
 
 resource "aws_sns_topic" "example3" {
   kms_master_key_id = "value"
